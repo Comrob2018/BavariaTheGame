0096964f7951fe205a6f989817a7c102f99c0bd2
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2019-06-11 14:51:47 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -9,9 +9,8 @@ def WrappedTextOutput(p_text, p_width=70):
     print(s)
     
 def clear_screen():
-  print('\033[H\033[J')
   print('\n'*250)
-  print('\033[J\033[H') #moves the cursor back to the top
+  print('\033[H\033[J') #moves the cursor back to the top
   time.sleep(0.1)
 
-"""
+
