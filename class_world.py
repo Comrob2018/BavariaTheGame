191618cb17ebc426caca9fa5ee4b1b9033a72475
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2019-06-11 09:54:21 -0500

    Update class_world.py

--- class_world.py
+++ class_world.py
@@ -39,7 +39,7 @@ class gameWorld:
 
     #Creating Item list
     self.Spells= [class_support.Spell("Cure", 1, "Heal", 15, 30), class_support.Spell("Fireball", 1, "Atk", 15, 15), class_support.Spell("Str Up", 1, "Buff", 20, 5)]
-    self.Skills= [class_support.Skill("", "Atk", , ), class_support.Skill("", "Atk", , ), class_support.Skill("", "Atk", , ), class_support.Skill("", "Atk", , ), class_support.Skill("", "Atk", , ), class_support.Skill("", "Atk", , )
+    self.Skills= [class_support.Skill("Cleaving Strike", "Atk", 20, 30), class_support.Skill("Mug", "Atk", 5, 15), class_support.Skill("Whirlwind Strike", "Atk", 15, 20), class_support.Skill("Death Blow", "Atk", 40, 65), class_support.Skill("Steal Life", "Atk", 10, 20)]
     self.weapons = [class_support.Weapon("Short Sword", 5, 10, "sword"), class_support.Weapon("Brass Knuckles", 10, 14, "bare"), class_support.Weapon("Wooden Staff", 5, 10, "staff"), class_support.Weapon("Rapier", 9, 5, "sword"), class_support.Weapon("Long Sword", 20, 10, "sword"), class_support.Weapon("light Axe", 28, 15, "axe"), class_support.Weapon("Small Knife", 5, 15, "knife"), class_support.Weapon("Stick", 1, 3, "knife")]
     self.armors = [class_support.Armor("Cloth Armor", 10, 5, "cloth"), class_support.Armor("Shirt", 10, 3, "cloth"), class_support.Armor("Leather Armor", 15, 8, "leather"), class_support.Armor("Robe", 10, 3, "cloth"), class_support.Armor("Cover-all", 5, 7, "cloth")]
     self.items = [class_support.Item("Potion", "Heal", 1, 30), class_support.Item("Ether", "Heal", 1, 25),class_support.Item("Kings Crown", "Key", 1), class_support.Item('Door Key', "Consumable", 1), class_support.Item("Skull Key", "Consumable", 1)]
